15a421d15176ce84e0aac06674af277bd7f8d70b
gerben <vandeveldegerben@gmail.com>
2023-10-27 10:07:10 +0200

    fix interface vragen

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
-ant = int(input("Wil je server toevoegen = 1 verwijderen = 2 lijst tonen = 3"))
+import sys
+if __name__=='__main__':
+    if len(sys.argv > 1):
+        ant = sys.argv
+        if ant == 1:
+            print("U wilt een server toevoegen")
+        elif ant == 2:
+            print("U wilt een server verwijderen")
+        elif ant == 3:
+            print("U heeft gekozen voor de lijst te tonen")
+        else:
+            print("Geef een geldig ant")
 
-if ant == 1:
-    print("U wilt een server toevoegen")
-elif ant == 2:
-    print("U wilt een server verwijderen")
-elif ant == 3:
-    print("U heeft gekozen voor de lijst te tonen")
-else:
-    print("Geef een geldig ant")
