ac0a827ba99d6a9f1aef1e6c6bb77607c673083d
gerben <vandeveldegerben@gmail.com>
2023-10-27 09:56:29 +0200

    eerste bulletpoint maken

---
+++ server.py
@@ -0,0 +1,10 @@
+ant = int(input("Wil je server toevoegen = 1 verwijderen = 2 lijst tonen = 3"))
+
+if ant == 1:
+    print("U wilt een server toevoegen")
+elif ant == 2:
+    print("U wilt een server verwijderen")
+elif ant == 3:
+    print("U heeft gekozen voor de lijst te tonen")
+else:
+    print("Geef een geldig ant")
